0d462a6aa947c26a7dcefd1c03c8a8ec8b1ca316
saidanrabia1-sys <saidanrabia1@gmail.com>
2025-12-04 16:05:30 +0300

    création des variables et d'un fichier nommé gestionBibliotheque

---
+++ gestionBibliotheque.js
@@ -0,0 +1,14 @@
+// Je crée une variable initialisée à 500 
+
+let totalLivres = 500;
+totalLivres+= 50 // J'ai rajouter 50 livres de plus 
+totalLivres-= 10 // J'ai jeter 10 livres 
+totalLivres+= 5 // J'ai racheter 5 livres jetés
+console.log(totalLivres);
+
+// Je crée 
+
+let affichageTotalLivres = "Le nombre totale de livres est de ";
+affichageTotalLivres+= totalLivres
+console.log(affichageTotalLivres);
+
---
+++ script.js
@@ -0,0 +1,62 @@
+// Ici je redige mon script JavaScript
+
+let nom ="SAID";
+        let prenom ="Anrabia";
+        console.log("Bonjour,je m'appelle",prenom,nom);
+
+        //je declare mon age
+
+        let age = 18;
+        console.log("J'ai",age);
+
+        let prix = 12;
+        const nombreDecimale = 3.4123;
+        const nombreNegatif = -509;
+        const nombreFraction = 1/3;
+        console.log("prix: ",prix);
+        console.log("nombre décimale: ",nombreDecimale);
+        console.log("nombre négatif: ",nombreNegatif);
+        console.log("nombre fraction: ",nombreFraction);
+
+        let salutation = "Bonjour les gens";
+        console.log(salutation);
+
+        let greeting = 'Good morning';
+        console.log(greeting);
+
+        let andabu = 'kwezi';
+        console.log(andabu);
+
+        let commune ="M'tsamboro";
+        console.log(commune);
+
+        let village = 'M\'tsahara';
+        console.log(village);
+
+        let vrai = true;
+        console.log(vrai)
+
+        let est_faux = false;
+        console.log(est_faux)
+
+        // je déclare un ableau
+        let eleves = ["Said","Anraabia","Mohamed","Anzilati"];
+        console.log(eleves);
+        console.log(eleves[0]); //Je récupère le 1er membre du tableau
+        console.log(eleves[1]); //Je récupère le 2ème membre du tableau
+        console.log(eleves[2]); //Je récupère le 3ème membre du tableau
+        console.log(eleves[3]); //Je récupère le 4ème membre du tableau
+
+        let diplome = [true,10,"Roukaya"];
+        console.log(diplome);
+
+        let client = {
+            nom: "Said",
+            village: "kaweni",
+            age : 18,
+            score: [4,7,2]
+        };
+        console.log(client);
+
+        
+    
