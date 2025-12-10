bb89757bea76b9a20e18379971d4177ad1be9470
saidanrabia1-sys <saidanrabia1@gmail.com>
2025-12-10 16:34:53 +0300

    création d'un fichier

---
+++ gestionticketcinema.js
@@ -0,0 +1,15 @@
+// Déclaration de l'objet ticket
+let ticket = {
+    nomFilm: "The Walking Dead",
+    prix: 16,
+    numeroSalle: 2
+};
+
+// Déclaration de la variable nom
+let nom1 = "Anrabia"; // <-- remplacez par votre vrai nom
+
+// Création du texte à afficher
+let texteAffichage = `Bonjour ${nom1}, votre film ${ticket.nomFilm} est en salle ${ticket.numeroSalle}`;
+
+// Affichage dans la console
+console.log(texteAffichage);
