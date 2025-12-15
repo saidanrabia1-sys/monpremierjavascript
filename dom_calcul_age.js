60ca82a49333ffd800c0f81c7281ce3d0e25f9c0
unknown <saidanrabia1@gmail.com>
2025-12-15 16:43:07 +0300

    création des fichier et calcul d'age

--- dom_calcul_age.js
+++ dom_calcul_age.js
@@ -1,13 +1,48 @@
 // Récupération des éléments du DOM
 function calculAge() {
 const anneeNaissance = document.getElementById("anneeNaissance").value;
-const ageAffichage = document.getElementById("ageResultat");
+const ageResultat = document.getElementById("ageResultat");
 
-    if (anneeNaissance && isNaN (anneeNaissance)) {
+    if (isNaN (anneeNaissance) > 1900  && anneeNaissance <= new Date().getFullYear()){
         const anneeCourante = new Date().getFullYear();
-        const age = anneeCourante - parseInt (anneeNaissance);
-        ageAffichage.textContent = age + " ans";
+        const age = anneeCourante - anneeNaissance;
+        ageResultat.textContent = age + " ans";
     } else {
-        ageAffichage.textContent = "";
+        ageResultat.textContent = "";
     }
 }
+/*Je récupère un élément (une balise) situé dans le fichier HTML
+(calcule_age.html). J'utilise l'identifiant pour trouver le bon élément.
+L'élément récupéé est ensuite stocké dans la variable
+elementBouttonValider
+*/
+let elementBouttonValider = document.getElementById("bouttonValider");
+elementBouttonValider.addEventListener("click",function(){
+    console.log("elementBouttonvalider :", elementBouttonValider)
+});
+console.log(document.getElementById("nom"));
+
+// J'affiche l'élément récupéré dans la console
+let elementPrenom = document.getElementById("prenom");
+elementPrenom.addEventListener("click",function(){
+    console.log("prenom :", elementPrenom)
+});
+console.log(document.getElementById("prenom"));
+
+
+let elementAnneeNaissance = document.getElementById("anneeNaissance");
+elementAnneeNaissance.addEventListener("click",function(){
+    console.log("elementAnneeNaissance :", elementAnneeNaissance)
+});
+console.log(document.getElementById("anneeNaissance"));
+
+console.log(document.head);
+
+console.log(document.title);
+
+// Je récupère lélément HTML (balis) dont l'identifiant est recensement 
+let elementDivRecensement = document.querySelector("#recensement");
+console.log("elementDivRecensement :", elementDivRecensement);
+
+let elementPRecensement = document.querySelector("#paragraphe1");
+console.log(elementPRecensement);
