7d4a61f89b6740929a03d9c24c2072f5be79cf79
unknown <saidanrabia1@gmail.com>
2026-01-07 09:21:59 +0300

    renitialisation de ma page web

--- manipulationDate.js
+++ manipulationDate.js
@@ -6,27 +6,37 @@ const maDate = new Date();
 
 const anneeEnCours  = maDate.getFullYear();
 
+//Affichage de l'année 
 let elementSpanAnnee = document.querySelector("#annee");
 elementSpanAnnee.textContent = anneeEnCours;
 
+//Affichage de mois 
 let elementSpanMois = document.querySelector("#mois");
 elementSpanMois.textContent = maDate.getMonth();
 
-
-const dateComplete = date.toLocaleDateString("fr-FR", {
-  day: "numeric",
-  month: "long",
-  year: "numeric"
-});
+//Affichage de la date complète
+let dateComplete = document.getElementById("dateComplete");
+dateComplete.textContent = maDate.toLocaleDateString();
 
 console.log(dateComplete);
 
-
-let elementSpanHeureMayotte = document.querySelector("#heureMayotte");
-elementSpanHeureMayotte = maintenant.toLocaleDateString("fr-FR");
-
-const options = {
-    hour: "2-digit",
-    minute: "2-digit", 
-    second:"2-digit",
+// Affichage heure de mayotte 
+function afficherHeure() {
+  document.getElementById("heureMayotte").textContent =
+    new Date().toLocaleTimeString("fr-FR");
 }
+
+afficherHeure();
+setInterval(afficherHeure, 1000);//J'exécute une fonction à intervalles de temps réguliers 
+
+//Affichage heure de paris 
+function afficherHeureParis() {
+    const maintenant = new Date().toLocaleTimeString("fr-FR", {
+      timeZone: "Europe/Paris"
+    });
+    document.getElementById("heureParis").textContent = maintenant;
+  }
+
+  setInterval(afficherHeureParis, 1000);//setInterval permet de bien afficher l'heure a temps 
+  afficherHeureParis();
+
